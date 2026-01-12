0cfd2a0083b3f214ea42f944005bc650dcd75bb8
limtaesik <limtaesik@gmail.com>
2026-01-12 19:35:28 +0900

    fix: i18n syntax + default export

--- lib/i18n.ts
+++ lib/i18n.ts
@@ -3,3 +3,8 @@ export const locales = ["en", "ko", "ja", "zh"] as const;
 export type Locale = (typeof locales)[number];
 
 export const defaultLocale: Locale = "en";
+
+export default {
+  locales,
+  defaultLocale,
+};
