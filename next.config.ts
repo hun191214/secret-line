61c39104de9fd82e824c5d486f909ae46ecab2a4
limtaesik <limtaesik@gmail.com>
2026-01-13 13:03:32 +0900

    Fix: Resolve Vercel 500 error by isolating backend dependencies

--- next.config.ts
+++ next.config.ts
@@ -5,6 +5,16 @@ const withNextIntl = createNextIntlPlugin('./i18n/request.ts');
 
 const nextConfig: NextConfig = {
   // 기존 설정 유지
+  webpack: (config) => {
+    config.plugins = config.plugins || [];
+    // 백엔드 관련 폴더/파일 번들 제외
+    config.plugins.push(
+      new config.webpack.IgnorePlugin({
+        resourceRegExp: /(_backup_hold|nest|nestjs|server|backend)/
+      })
+    );
+    return config;
+  }
 };
 
 export default withNextIntl(nextConfig);
